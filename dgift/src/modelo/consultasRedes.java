af90848bd8f92c9ba1b8631e44e65adcc670b23b
user <user@DESKTOP-NJBPVFC>
2021-09-22 04:28:41 -0300

    consulta redes implementado metodos

--- dgift/src/modelo/consultasRedes.java
+++ dgift/src/modelo/consultasRedes.java
@@ -6,6 +6,7 @@
 package modelo;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -15,23 +16,82 @@ import java.sql.SQLException;
 public class consultasRedes extends conectDb {
 
      
-    public boolean registar(redes red) throws SQLException {
-        
+    public boolean registar(redes red) throws SQLException { 
      PreparedStatement ps = null;   
-     
      Connection con =  getConexion();
-     
      String sql = "INSERT INTO redes ( nombre_red, es_activo ) VALUES (?,?)";
      
-     ps = con.prepareStatement(sql);
+       try{
+       ps = con.prepareStatement(sql);
+       ps.setString(1, red.getNombre_red());
+       ps.setInt(2, red.getEs_activo()); 
+       ps.execute();
+       return true;
+       }catch(SQLException e){
+       System.err.println(e);
+           return false;
+       }finally{
+         try{
+           con.close();
+          }catch(SQLException e){
+             System.err.println(e);
+          }
+       } 
      
+    }
+    
+    
+    public boolean modificar(redes red) throws SQLException { 
+     PreparedStatement ps = null;   
+     Connection con =  getConexion();
+     String sql = "UPDATE redes SET nombre_red=?, es_activo=? WHERE id_rrss=?";
+     
+       try{
+       ps = con.prepareStatement(sql);
+       ps.setString(1, red.getNombre_red());
+       ps.setInt(2, red.getEs_activo());
+       ps.setInt(3, red.getId_rrss());
+       ps.execute();
+       return true;
+       }catch(SQLException e){
+       System.err.println(e);
+           return false;
+       }finally{
+         try{
+             con.close();
+          }catch(SQLException e){
+             System.err.println(e);
+          }
+       } 
+     
+    }
+    
+    
+    public boolean buscar(redes red) throws SQLException { 
+     PreparedStatement ps = null;   
+     ResultSet rs =null;
+     Connection con =  getConexion();
+     String sql = "SELECT * FROM redes WHERE id_rrss=?";
+     
+       try{
+       ps = con.prepareStatement(sql);
+       ps.setString(1, red.getNombre_red());
+       ps.setInt(2, red.getEs_activo()); 
+       rs = ps.executeQuery();
        
+       
+       return true;
+       }catch(SQLException e){
+       System.err.println(e);
+           return false;
+       }finally{
+         try{
+           con.close();
+          }catch(SQLException e){
+             System.err.println(e);
+          }
+       } 
      
-       ps.setInt(1, red.getId_rrss());
-        return false;
-     
-      
-        
     }
 
 }
