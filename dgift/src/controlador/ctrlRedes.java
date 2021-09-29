b1c0bc2f2da197a7195dbbaf57e52ef03d37f400
user <user@DESKTOP-NJBPVFC>
2021-09-29 02:50:24 -0300

    listo redes..!

--- dgift/src/controlador/ctrlRedes.java
+++ dgift/src/controlador/ctrlRedes.java
@@ -5,14 +5,18 @@
  */
 package controlador;
 
+
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import modelo.redesDAO;
 import modelo.redes;
@@ -33,17 +37,18 @@ public class ctrlRedes implements ActionListener{
     
     
 
-    public ctrlRedes(fMaestrosMarco f) {
-      this.frm = f;
+    public ctrlRedes(fMaestrosMarco f) throws SQLException {
+        this.frm = f;
         this.frm.btnCancelarRedes.addActionListener(this);
         this.frm.btnEditarRedes.addActionListener(this);
         this.frm.btnGuardarRedes.addActionListener(this);
         this.frm.btnSalirRedes.addActionListener(this);
-        this.frm.txtRed.addActionListener(this);
-        this.frm.chkActivoRedes.addActionListener(this);
-        this.frm.txtBusquedaRedes.addActionListener(this);
-        this.frm.txtIdRrss.addActionListener(this);
-        listarP(frm.grRedes);
+        this.frm.btnBusquedaRedes.addActionListener(this);
+        this.frm.grRedes.setRowSelectionAllowed(true);
+        this.frm.grRedes.setColumnSelectionAllowed(false); 
+        this.frm.txtIdRrss.setVisible(false);
+        red.setNombre_red(frm.txtBusquedaRedes.getText());
+        listarBusq(frm.grRedes,red);
     }
 
     
@@ -56,9 +61,13 @@ public class ctrlRedes implements ActionListener{
           try {
               if(redC.registar(red)){
                   limpiarPantalla();
+                  Toolkit.getDefaultToolkit().beep();
                   JOptionPane.showMessageDialog(null, "Registro Guardado Exitosamente");
-                  listarP(frm.grRedes);
+                  frm.txtBusquedaRedes.setText(null);
+                  red.setNombre_red(frm.txtBusquedaRedes.getText());
+                  listarBusq(frm.grRedes,red);
               }else{
+                  Toolkit.getDefaultToolkit().beep();
                   JOptionPane.showMessageDialog(null, "Error al guardar");           
               } } catch (SQLException ex) {
               Logger.getLogger(ctrlRedes.class.getName()).log(Level.SEVERE, null, ex);
@@ -72,15 +81,31 @@ public class ctrlRedes implements ActionListener{
       
           try {
               if(redC.modificar(red)){
-                  limpiarPantalla();
+                  Toolkit.getDefaultToolkit().beep();
                   JOptionPane.showMessageDialog(null, "Registro Editado Exitosamente");
-                  listarP(frm.grRedes);
+                  red.setId_rrss(Integer.parseInt(frm.txtIdRrss.getText()));
+                  listarBusq(frm.grRedes,red);
+                  limpiarPantalla();
               }else{
+                  Toolkit.getDefaultToolkit().beep();
                   JOptionPane.showMessageDialog(null, "Error al Editar");           
               } } catch (SQLException ex) {
               Logger.getLogger(ctrlRedes.class.getName()).log(Level.SEVERE, null, ex);
           }
        } 
+       
+       if(e.getSource()== frm.btnBusquedaRedes){
+       
+       red.setNombre_red(frm.txtBusquedaRedes.getText());
+          try {
+              
+              listarBusq(frm.grRedes,red);
+               } catch (SQLException ex) {
+              Logger.getLogger(ctrlRedes.class.getName()).log(Level.SEVERE, null, ex);
+          }
+       } 
+       
+       
     }
     
     public void limpiarPantalla(){
@@ -92,7 +117,7 @@ public class ctrlRedes implements ActionListener{
         frm.btnGuardarRedes.setEnabled(false);
     }
     
-    public void listarP (JTable listRedes){
+   /* public void listarP (JTable listRedes){
         
         modeloRed=(DefaultTableModel)listRedes.getModel();
         modeloRed.getDataVector().removeAllElements();
@@ -107,6 +132,35 @@ public class ctrlRedes implements ActionListener{
          object[2]= (lista.get(i).getEs_activo()==1)?Boolean.TRUE:Boolean.FALSE;
          modeloRed.addRow(object);
         }
+        frm.grRedes.setModel(modeloRed);
+        
+    }*/
+    
+    
+     public void listarBusq (JTable listRedes,redes red) throws SQLException{
+        
+        modeloRed=(DefaultTableModel)listRedes.getModel();
+        modeloRed.getDataVector().removeAllElements();
+        modeloRed.fireTableDataChanged();
+        
+        List<redes>lista= redC.buscar(red);
+        Object[]object = new Object[3];
+        int i;
+        if (lista.size()==0){
+        Toolkit.getDefaultToolkit().beep(); 
+        JOptionPane.showMessageDialog(null, "No se encontraron Registros Asociados a su busqueda","Aviso", JOptionPane.WARNING_MESSAGE);
+         
+        }
+        for(i=0;i<lista.size();i++){
+         object[0]= lista.get(i).getId_rrss();
+         object[1]= lista.get(i).getNombre_red();
+         object[2]= (lista.get(i).getEs_activo()==1)?Boolean.TRUE:Boolean.FALSE;
+         modeloRed.addRow(object);
+        }
+        
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+        frm.grRedes.getColumnModel().getColumn(0).setCellRenderer( centerRenderer );
         frm.grRedes.setModel(modeloRed);
         
     }
